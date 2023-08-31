9315479b3ccc481c35c828f81f583f6f24ce91ee
SouravBarman001 <sourav.iit.nstu@gmail.com>
2023-08-31 19:34:01 +0600

    folder structure added with auth,category,home and bottom nav

---
+++ lib/src/core/network/network.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:network/network.dart';
+
+final dioProvider = Provider(
+  (ref) {
+    return RestClient(
+      baseUrl: 'https://fakestoreapi.com/',
+      token: '',
+    );
+  },
+);
+
+
+final dioUserListProvider = Provider(
+      (ref) {
+    return RestClient(
+      baseUrl: 'https://fakestoreapi.com/',
+      token: '',
+    );
+  },
+);
